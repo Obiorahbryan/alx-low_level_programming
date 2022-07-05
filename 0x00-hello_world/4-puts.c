678914f89c7c9a4d70d6368c890dd761152a6ded
odogwunamalayyyy <ebival4reel@gmail.com>
2022-07-05 08:03:22 -0700

    task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdioi.h>
+
+/**
+ * main - prints exactly a sentence
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	put("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
