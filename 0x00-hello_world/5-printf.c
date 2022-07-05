4acb45fa356a55e6eaaf3354582ba95ea0140362
odogwunamalayyyy <ebival4reel@gmail.com>
2022-07-05 08:10:44 -0700

    task done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - write a C program to print using printf
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
