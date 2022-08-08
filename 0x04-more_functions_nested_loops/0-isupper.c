d364ddedf775fdd6b8aac7dea4815133dbde2de9
odogwunamalayyyy <ebival4reel@gmail.com>
2022-08-08 04:47:35 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- *  _isuppper -> this is a function to check upper case alphas
- *  @c: an integer paramer
- *  Return: something but success
- */
+ * _isupper - this function says if a character is upper or not.
+ * @c: the character to be process
+(* a blank line
+* Description: this function says if a character is upper or not)?
+(* section header: the header of this function is holberton.h)*
+* Return: return 1 if c is upper and 0 in other cases.
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	else 
+	else
 		return (0);
 }
