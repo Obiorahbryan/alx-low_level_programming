8dcdb36d3d76364fdbadffc84cf23a9c8a4ce9f3
odogwunamalayyyy <ebival4reel@gmail.com>
2022-08-12 23:32:19 -0700

    task done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
-* _strlen_recursion -> function to get length of string
-* @s: string param
-* Return: length of string
-*/
+ * _strlen_recursion - note: no _putchar in local test
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 int _strlen_recursion(char *s)
 {
-	static int count=0;
 	if (*s == '\0')
-	{
-		return count;
-	}
-	count += 1;
-	s++;
-	_strlen_recursion(s);
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
