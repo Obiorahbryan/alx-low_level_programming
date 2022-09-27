25b6d5d43938e117fd850d89d2f3d358d46c8241
obiorahbryan <ebival4reel@gmail.com>
2022-09-27 15:15:34 -0700

    task done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,15 +12,16 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX SE project
+ *
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
@@ -33,5 +35,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+int is_visited(const listint_t *, const listint_t **, int);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
