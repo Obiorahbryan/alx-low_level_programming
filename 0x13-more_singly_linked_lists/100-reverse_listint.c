ff91b40cb0a7ac9b3fdc646659dd5bc7301cbe64
obiorahbryan <ebival4reel@gmail.com>
2022-09-27 15:31:20 -0700

    task done

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,33 +1,25 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - adds a node to a singly linked list
- * @h: pointer to head of the list
- * @idx: position to delete
- * Return: pointer to new elemnt
+ * reverse_listint - reverses a linked list
+ * @h: head of the list
+ *
+ * Return: pointer to the reversed
  */
-int delete_nodeint_at_index(listint_t **h, unsigned int idx)
+listint_t *reverse_listint(listint_t **h)
 {
-	unsigned int i = 0;
-	listint_t *tmp = *h, *head = *h;
+	listint_t *tmp2, *tmp1;
 
-	if (!head || !h)
-		return (-1);
-	if (idx == 0)
+	if (!h || !(*h))
+		return (NULL);
+	tmp1 = (*h)->next;
+	(*h)->next = NULL;
+	while (tmp1)
 	{
-		*h = head->next;
-		free(head);
-		return (1);
+		tmp2 = *h;
+		*h = tmp1;
+		tmp1 = (*h)->next;
+		(*h)->next = tmp2;
 	}
-	while (head && i < idx)
-	{
-		tmp = head;
-		head = head->next;
-		i++;
-	}
-	if (i < idx)
-		return (-1);
-	tmp->next = head->next;
-	free(head);
-	return (1);
+	return (*h);
 }
